577406758bdf69e8b0208d2e42369be74f9b5af0
Shahnawaz Alam <shahnawazshafiss9@gmail.com>
2021-01-25 12:57:38 +0530

    section3 completed

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import CommentDetails from './CommentDetails'
+import ApprovalCard from './ApprovalCard'
 
 const App = () => {
-  return <div>Hi there!</div>
+  return (<div className="ui container comments">
+    <ApprovalCard>
+      <CommentDetails author="Shahnawaz Alam" />
+    </ApprovalCard>
+    <ApprovalCard>
+      <CommentDetails author="Sher Singh" />
+    </ApprovalCard>
+    <ApprovalCard>
+      <CommentDetails author="Soni Priya" />
+    </ApprovalCard>
+  </div>)
 }
 ReactDOM.render(<App />, document.querySelector('#root'));
